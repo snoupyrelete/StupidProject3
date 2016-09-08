61e0f79a8fdd9ec24028f38dcf353939262e9b7d
snoupyrelete <drob8896@740s-pl-112.local>
2016-09-08 09:21:08 -0600

    Got text input from user.

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,17 +1,29 @@
 package stupid.controller;
 
+import java.util.Scanner;
+
 public class StupidController
 {
+	private Scanner inputReader; // Data members
 	private String myName;
 
 	public StupidController() // Constructor
 	{
-		myName = "Dylan Robson"; // Initializes a string
+		myName = "Dylan Robson"; // Initializes Data members
+		inputReader = new Scanner(System.in);
 	}
 
-	public void start()
+	public void start() // Methods
 	{
 		System.out.println("This program is slightly less stupid.");
 		System.out.println("My name is " + myName);
+		askQuestions();
+	}
+	
+	private void askQuestions()
+	{
+		System.out.println("How is your day?");
+		String input = inputReader.nextLine();
+		System.out.println("You're having a " + input + " day.");
 	}
 }
--- src/stupid/controller/StupidRunner.java
+++ src/stupid/controller/StupidRunner.java
@@ -1,10 +1,11 @@
 package stupid.controller;
 
-public class StupidRunner 
+public class StupidRunner
 {
-	public static void main (String [] args) 
-	{ 
+	public static void main(String[] args)
+	{
 		StupidController firstAppController = new StupidController(); // Constructor call
+																	
 		firstAppController.start(); // Method call
 	}
 }
