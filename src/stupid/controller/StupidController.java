b76beedf488b61496684604c08a5bd49d441a362
snoupyrelete <drob8896@740s-pl-112.local>
2016-09-08 08:50:41 -0600

    Prints myName

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,5 +1,17 @@
 package stupid.controller;
 
-public class StupidController {
+public class StupidController
+{
+	private String myName;
 
+	public StupidController() // Constructor
+	{
+		myName = "Dylan Robson"; // Initializes a string
+	}
+
+	public void start()
+	{
+		System.out.println("This program is slightly less stupid.");
+		System.out.println("My name is " + myName);
+	}
 }
--- src/stupid/controller/StupidRunner.java
+++ src/stupid/controller/StupidRunner.java
@@ -1,5 +1,10 @@
 package stupid.controller;
 
-public class StupidRunner {
-
+public class StupidRunner 
+{
+	public static void main (String [] args) 
+	{ 
+		StupidController firstAppController = new StupidController(); // Constructor call
+		firstAppController.start(); // Method call
+	}
 }
