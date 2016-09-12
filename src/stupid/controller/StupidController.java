d88e0db874de5b9772893d4cefb94992ecd22d7b
snoupyrelete <drob8896@740s-pl-112.local>
2016-09-12 08:35:59 -0600

    Added 8 new questions handling I/O.

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -23,7 +23,44 @@ public class StupidController
 	private void askQuestions()
 	{
 		System.out.println("How is your day?");
-		String input = inputReader.nextLine();
-		System.out.println("You're having a " + input + " day.");
+		String dayInput = inputReader.nextLine();
+		System.out.println("You're having a " + dayInput + " day.");
+		
+		System.out.println("What is the best type of food?");
+		String foodInput = inputReader.nextLine();
+		System.out.println("You said that " + foodInput + " is the best type of food.");
+		
+		System.out.println("What is your favorite color?");
+		String colorInput = inputReader.next();
+		System.out.println("You said that " + colorInput + " is your favorite color.");
+	
+		System.out.println("How old are you?");
+		int ageInput = inputReader.nextInt();
+		System.out.println("You said that you are " + ageInput + " years old.");
+		
+		System.out.println("How hot are you out of 10 as a decimal?");
+		float hotnessInput = inputReader.nextFloat();
+		System.out.println("You're an " + hotnessInput + " out of 10!.");
+		
+		System.out.println("What are your physical attributes?");
+		String physicalInput = inputReader.nextLine();
+		System.out.println("You said that you are " + physicalInput);
+	
+		System.out.println("What is your favorite activity to do?");
+		String activityInput = inputReader.nextLine();
+		System.out.println("You said that your favorite activity is " + activityInput);
+		
+		System.out.println("What size shoe do you wear?");
+		float shoeInput = inputReader.nextFloat();
+		System.out.println("You said that you wear a shoe size of: " + shoeInput);
+		
+		System.out.println("What is your first name?");
+		String nameInput = inputReader.next();
+		System.out.println("Your name is: " + nameInput);
+		
+		System.out.println("What is the best brand of car?");
+		String carInput = inputReader.next();
+		System.out.println("You said that " + carInput + " is the best type of car.");
+	
 	}
 }
